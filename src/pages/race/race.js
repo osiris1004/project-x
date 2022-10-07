5452d86bc1c337c59b5f43ef6ed5ad143f4833bc
MaesTro <kontchou05@gmail.com>
2022-10-07 09:06:50 +0200

    modal for both button working simultaniously

--- src/pages/race/race.js
+++ src/pages/race/race.js
@@ -1,10 +1,20 @@
-function myFunction() {
+function countryClick() {
     document.querySelector("#main").classList.toggle("show-main") 
-    document.getElementById("myDropdown").classList.toggle("show");
+    document.querySelector(".myDropDownCountry").classList.toggle("show");
 }
 window.onclick = function (event) {
-    if (!event.target.matches('.dropbtn') ) {
+    
+    if (!event.target.matches('.dropDownCountry')) {
         document.querySelector('#main').classList.remove('show-main')
-        document.querySelector(".dropdown-content").classList.remove('show');
+        document.querySelector(".myDropDownCountry").classList.remove('show');
     }
+    if (!event.target.matches('.dropdownCoat') ) {
+                document.querySelector('#main').classList.remove('show-main-coat')
+                document.querySelector(".myDropDownCoat").classList.remove('show-coat');
+            }
+}
+
+function coatClick() {
+    document.querySelector("#main").classList.toggle("show-main-coat") 
+    document.querySelector(".myDropDownCoat").classList.toggle("show-coat");
 }
